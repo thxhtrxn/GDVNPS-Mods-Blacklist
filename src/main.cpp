615ea6f49c66db7ddc9064dd68c56a3b8c0e52fa
thxhtrxn <tranxuanthanhreal@gmail.com>
2025-05-03 17:13:42 +0700

    patch

--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,37 @@
 
 using namespace geode::prelude;
 
-bool shouldShowPopup = false;
+bool shouldForceCheckMods = false;
 
-// You know what? Fuck me.
+const std::unordered_set<std::string> blacklistedMods = {
+    "beat.shipcopter",
+    "raydeeux.frickglow",
+    "ziegenhainy.fungal_shift",
+    "covernts.custom_wraith_codes",
+    "capeling.gamemode-switcher",
+    "hiimjustin000.fake_rate",
+    "bitz.moregames",
+    "raydeeux.antilobotomy",
+    "alphalaneous.asyncweb",
+    "raydeeux_colon.towersecretending",
+    "mariomastr.customisable-speed-portals",
+    "camila314.pathfinder"
+};
+
 $on_mod(Loaded) {
-    // More will coming soon...
-    std::unordered_set<std::string> modList = {
-        "beat.shipcopter",
-        "raydeeux.frickglow",
-        "ziegenhainy.fungal_shift",
-        "covernts.custom_wraith_codes",
-        "capeling.gamemode-switcher",
-        "hiimjustin000.fake_rate",
-        "bitz.moregames",
-        "raydeeux.antilobotomy",
-        "alphalaneous.asyncweb",
-        "raydeeux_colon.towersecretending",
-        "mariomastr.customisable-speed-portals",
-        "camila314.pathfinder"
-    };
+    log::info("Running blacklist check...");
+    bool hasDisabled = false;
 
     for (auto const& mod : Loader::get()->getAllMods()) {
-        if (modList.find(mod->getID()) != modList.end()) {
-            if (mod->isEnabled()) {
-                mod->disable();
-                shouldShowPopup = true;
-            }
+        if (blacklistedMods.count(mod->getID()) && mod->isEnabled()) {
+            mod->disable();
+            log::info("Disabling {}", mod->getID());
+            hasDisabled = true;
         }
+    }
+
+    if (hasDisabled) {
+        shouldForceCheckMods = true;
     }
 }
 
@@ -39,26 +43,42 @@ class $modify(MyMenuLayer, MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) return false;
 
-        if(shouldShowPopup) {
-            this->runAction(CCSequence::createWithTwoActions(
-                CCDelayTime::create(0.05f),
-                CCCallFunc::create(this, callfunc_selector(MyMenuLayer::showBlacklistPopup))
-            ));
+        if (shouldForceCheckMods) {
+            this->runAction(
+                CCSequence::createWithTwoActions(
+                    CCDelayTime::create(0.5f),
+                    CCCallFunc::create(this, callfunc_selector(MyMenuLayer::forceDisableBlacklistMods))
+                )
+            );
         }
+
         return true;
     }
 
-    void showBlacklistPopup() {
-        this->runAction(
-            CCSequence::create(
-                CCDelayTime::create(1.0f),
-                CCCallFunc::create(this, callfunc_selector(MyMenuLayer::gameRestart)),
-                nullptr
-            )
-        );
+    void forceDisableBlacklistMods() {
+        log::info("Running post-load blacklist check...");
+        bool foundEnabled = false;
+
+        for (auto const& mod : Loader::get()->getAllMods()) {
+            if (blacklistedMods.count(mod->getID()) && mod->isEnabled()) {
+                mod->disable();
+                log::warn("Mod {} was re-enabled — force-disabling again.", mod->getID());
+                foundEnabled = true;
+            }
+        }
+
+        if (foundEnabled) {
+            this->runAction(
+                CCSequence::createWithTwoActions(
+                    CCDelayTime::create(0.1f),
+                    CCCallFunc::create(this, callfunc_selector(MyMenuLayer::gameRestart))
+                )
+            );
+        }
     }
 
     void gameRestart() {
+        log::info("Restarting game to finalize blacklist enforcement.");
         game::restart();
     }
 };
